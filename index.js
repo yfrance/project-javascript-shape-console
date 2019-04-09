77eb5e9041c7d7c5f73ab25d1135102d43cd6775
Yan France <yanfrance.mumu@gmail.com>
2019-04-09 10:06:18 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const rectangleWidth = 100;
+const rectangleLength = 20;
+
+const rectangleArea = rectangleWidth * rectangleLength;
+
+console.log(`${rectangleArea} = ${rectangleWidth} * ${rectangleLength}`);
