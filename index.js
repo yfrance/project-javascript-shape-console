62c77d50cd77311a8f671d5fc44ffb62984caf5e
Yan France <yanfrance.mumu@gmail.com>
2019-04-10 00:15:26 +0700

    change index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const rectangleWidth = 100;
-const rectangleLength = 20;
+const sideA = 25;
+const sideB = 20;
+const sideC = 15;
+const perimeter = (sideA + sideB + sideC) / 2;
+const area = Math.sqrt(
+  perimeter * ((perimeter - sideA) * (perimeter - sideB) * (perimeter - sideC))
+);
 
-const rectangleArea = rectangleWidth * rectangleLength;
-
-console.log(`${rectangleArea} = ${rectangleWidth} * ${rectangleLength}`);
+console.log(area);
